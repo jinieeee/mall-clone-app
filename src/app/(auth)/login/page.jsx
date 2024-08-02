64d9fd46f9410458d1e951daf1b90245c6ecdf80
jinieeee <lje911@gmail.com>
2024-08-03 00:28:59 +0900

    [feat] css for login ui

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import LoginClient from "./LoginClient";
 
 const Login = () => {
-    return (
-        <div>
 
-        </div>
+    return (
+        <LoginClient />
     );
 };
 
