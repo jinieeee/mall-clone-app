5cdabc2431c2cb096d29149a693d01032caf0fb3
jinieeee <lje911@gmail.com>
2024-08-03 14:28:29 +0900

    [feat] create icon component

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -8,6 +8,7 @@ import {useRouter} from "next/navigation";
 
 import styles from "./Auth.module.scss";
 import Loader from "@/components/loader/Loader";
+import Input from "@/components/input/Input";
 
 const LoginClient = () => {
     const [email, setEmail] = useState('');
@@ -32,15 +33,36 @@ const LoginClient = () => {
 
     return (
         <>
-            <Loader />
+        {isLoading && <Loader />}
             <section className={styles.page}>
                 <div className={styles.container}>
                     <h1 className={styles.logo}>
-                        <Image priority src={LogoPath} />
+                        <Image priority src={LogoPath} alt='logo'/>
                     </h1>
                     <form onSubmit={loginUser} className={styles.form}>
-                        Input
                         {/* Input */}
+                        <Input
+                            email
+                            icon='letter'
+                            id='email'
+                            name='email'
+                            label='이메일'
+                            placeholder='아이디(이메일)'
+                            className={styles.control}
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <Input
+                            password
+                            icon='lock'
+                            id='password'
+                            name='password'
+                            label='비밀번호'
+                            placeholder='비밀번호'
+                            className={styles.control}
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
                         <div className={styles.group}>
                             {/* 자동 로그인, 비밀번호 수정 */}
                             자동 로그인, 비밀번호 수정
